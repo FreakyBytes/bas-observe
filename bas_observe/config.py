65c286c890420c14ae7af34e4be8a71e851c3630
Martin Peters <martin@freakybytes.net>
2017-12-11 18:43:02 +0100

    added connection management

--- bas_observe/config.py
+++ bas_observe/config.py
@@ -8,6 +8,10 @@ import logging
 import urllib.parse
 
 from attr import attrs, attrib
+import pika
+import influxdb
+
+from .queue import declare_amqp_pipeline
 
 
 @attrs
@@ -19,102 +23,73 @@ class Config(object):
         amqp_url            URL to the AMQP/RabbitMQ server
         influxdb_url        URL to the InfluxDB server
 
-    Properties:
-        amqp_host
-        amqp_port
-        influxdb_host
-        influxdb_port
-        influxdb_db
     """
 
-    project_name = attrib()  # type: string
-    amqp_url = attrib()  # type: string
-    influxdb_url = attrib()  # type: string
+    project_name = attrib()  # type: str
+    amqp_url = attrib()  # type: str
+    influxdb_url = attrib()  # type: str
 
     def __init___(self, *args, **kwargs):
         super(Config, self).__init__(*args, **kwargs)
 
-        self._amqp_host = None
-        self._amqp_port = None
+        self._amqp_connection = None
+        self._influxdb_connection = None
 
-        self._influxdb_proto = None
-        self._influxdb_host = None
-        self._influxdb_port = None
-        self._influxdb_db = None
-
-    def _parse_amqp_url(self):
-        url = urllib.parse.urlparse(self.amqp_url)
-        if url['scheme'] != 'amqp':
-            raise ValueError("Only amqp is supported as protocoll for the AMQP server at the moment")
-
-        self._amqp_host = url['hostname']
-        self._amqp_port = int(url['port']) or 5672
-
-    def _parse_influxdb_url(self):
+    def parse_influxdb_url(self):
         url = urllib.parse.urlparse(self.amqp_url)
         if url['scheme'] not in ('http', 'https', 'udp'):
             raise ValueError("Only http, https, and udp are supported as protocoll for InfluxDB")
 
-        self._influxdb_proto = url['scheme']
-        self._influxdb_host = url['hostname']
+        result = {
+            'scheme': url['scheme'],
+            'host': url['hostname'],
+            'user': url['username'],
+            'pass': url['password'],
+        }
         if url['port']:
-            self._influxdb_port = int(url['port'])
+            result['port'] = int(url['port'])
         elif self._influxdb_proto == 'https':
-            self._influxdb_port = 443
+            result['port'] = 443
         elif self._influxdb_proto == 'http':
-            self._influxdb_port = 80
+            result['port'] = 8086
         elif self._influxdb_proto == 'udp':
-            self._influxdb_port = 4444
+            result['port'] = 4444
 
         if url['path']:
-            self._influxdb_db = url['path']
+            result['db'] = url['path']
         else:
-            self._influxdb_db = 'bob_{name}'.format(name=self.project_name)
+            result['db'] = 'bob_{name}'.format(name=self.project_name)
 
-    @property
-    def amqp_host(self):
-        if not self._amqp_host:
-            self._parse_amqp_url()
+        return result
 
-        return self._amqp_host
+    def get_amqp_connection(self):
+        if not self._amqp_connection:
+            self._amqp_connection = pika.BlockingConnection(pika.URLParameters(self.amqp_url))
 
-    @property
-    def amqp_port(self):
-        if not self._amqp_port:
-            self._parse_amqp_url()
+        return self._amqp_connection
 
-        return self._amqp_port
+    def get_amqp_channel(self):
+        connection = self.get_amqp_connection()
+        channel = connection.get_channel()
+        # just in case declare the pipelines every time a new channel is opened
+        declare_amqp_pipeline(self, channel)
+        return channel
 
-    @property
-    def influxdb_proto(self):
-        if not self._influxdb_proto:
-            self._parse_influxdb_url()
+    def get_influxdb_connection(self):
+        if not self._influxdb_connection:
+            param = self.parse_influxdb_url()
+            self._influxdb_connection = influxdb.InfluxDBClient(
+                host=param['host'],
+                port=param['port'],
+                ssl=True if param['scheme'] == 'https' else False,
+                username=param['user'],
+                password=param['pass'],
+                database=param['db'],
+                use_udp=True if param['scheme'] == 'udp' else False,
+                udp_port=param['port']
+            )
 
-        return self._influxdb_proto
-
-    @property
-    def influxdb_host(self):
-        if not self._influxdb_host:
-            self._parse_influxdb_url()
-
-        return self._influxdb_host
-
-    @property
-    def influxdb_port(self):
-        if not self._influxdb_port:
-            self._parse_influxdb_url()
-
-        return self._influxdb_port
-
-    @property
-    def influxdb_db(self):
-        if not self._influxdb_db:
-            self._parse_influxdb_url()
-
-        if not self._influxdb_db:
-            return 'bob_{name}'.format(name=self.project_name)
-        else:
-            return self._influxdb_db
+        return self._influxdb_connection
 
 
 def setup_logging(level=logging.WARN, logfile=None):
---
+++ bas_observe/queue.py
@@ -0,0 +1,11 @@
+"""
+BAS Observe
+
+contains misc function to setup/configure AMQP message pipelines
+this is only to be invoked by the Config class
+"""
+
+
+def declare_amqp_pipeline(config, channel):
+    # TODO
+    pass

742c52fb4c190949a38f09ba6db863c5a7429453
Martin Peters <martin@freakybytes.net>
2017-12-18 11:12:52 +0100

    stump for addr analyser

---
+++ bas_observe/analyse/addr.py
@@ -0,0 +1,10 @@
+"""
+Analyser module, which compares the addresses of in coming packets with a table
+of known addresses
+"""
+
+from .base import BaseAnalyser
+
+
+class AddrAnalyser(BaseAnalyser):
+    pass
